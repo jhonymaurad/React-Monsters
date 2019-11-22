48f02fff6d68181a3b939ef10c932b1303e4ba24
jhony maurad <jhonymaurad@gmail.com>
2019-11-22 10:34:50 -0500

    Updates

---
+++ precache-manifest.d713612aeedff9385626ffd0d2f7728c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14e4e6b6fdf6fda3f3539dcf5c3f2ad",
+    "url": "/React-Monsters/index.html"
+  },
+  {
+    "revision": "9751b90635ba41d9d4ce",
+    "url": "/React-Monsters/static/css/main.0d422f18.chunk.css"
+  },
+  {
+    "revision": "cfaa617bb77cba7c8aec",
+    "url": "/React-Monsters/static/js/2.b2b32e4e.chunk.js"
+  },
+  {
+    "revision": "9751b90635ba41d9d4ce",
+    "url": "/React-Monsters/static/js/main.17b7d603.chunk.js"
+  },
+  {
+    "revision": "97d52635b5785349d38d",
+    "url": "/React-Monsters/static/js/runtime-main.f29fb490.js"
+  }
+]);
